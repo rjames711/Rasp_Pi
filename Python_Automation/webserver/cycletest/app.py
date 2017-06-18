80fb248183662b0cad399c514c73b4af417bde73
Rob Ames <amesrobert2@gmail.com>
2017-06-18 16:02:17 +0000

    added some notes about cycletest web app and cleaned up file a bit

--- Python_Automation/webserver/cycletest/app.py
+++ Python_Automation/webserver/cycletest/app.py
@@ -8,15 +8,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(pin, GPIO.OUT)
 
-@app.route("heyoo")
-def bitches():
-	return "<h2>heyoo<h2><br><h1>whats up bitches<h1>"
 
 @app.route("/")
 def index():
 	return render_template('index.html')
 
-
+#Turns on output if "on" is entered in name parameter otherwise turns off
 @app.route("/profile/<name>")
 def profile(name):
 	if name=='on':
@@ -27,8 +24,5 @@ def profile(name):
 
 
 if __name__ =="__main__":
-	print 'main is now '
 	app.run(host='0.0.0.0', port=80, debug=True)
-	for i in range (0,5):
-		print "heyoo"
    
